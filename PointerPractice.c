8ee9e83e5a6f22fb7cdd6a9a83bfca5272f77ee0
Zanir Habib <zanir.habib@gmail.com>
2019-12-06 01:03:20 -0800

    First run of pointerpractice

---
+++ PointerPractice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void swap(int* px, int py){
+	int temp;
+	temp = *px;
+	*px = *py;
+	*py = temp;
+        printf( 
+}
+
+int main()
+{
+    printf("The two numbers are %d and %d",2,3)
+    int a = 2;
+    int b = 3;
+    swap(&a,&b);
+    //printf("Hello world! - this is Zanir\n");
+    return 0;
+}
+
