5e637ae5efc8c27d0513507c53ccf960c21b90e8
Zanir Habib <zanir.habib@gmail.com>
2019-12-23 18:58:24 -0800

    Returning an array from a function

--- PointerPractice.c
+++ PointerPractice.c
@@ -3,8 +3,9 @@
 
 int* createArray(int size)
 {
-    int* a = malloc(sizeof(int) * size));
-    
+    int* a = malloc(sizeof(int) * size);
+    a[0] = 98;
+    return a; 
 
 
 
@@ -12,14 +13,26 @@ int* createArray(int size)
 
 int main()
 {
+    int* num = malloc(sizeof(int));
+    *num = 7;
+    int* list = createArray(*num);
+    for (int i = 0; i < *num; i++) 
+    {
+    printf("%d",list[i]);
+    }
+    printf("\n");
+    free(list);
+    free(num);
+    printf("ptr1 = %p,ptr2 = %p\n",(void *) list,(void *) num);
     
+    /*
     int a = 4;
     int *b = malloc(sizeof(int));
     *b = 10;
     printf("val = %d,ptr = %p\n",*b,(void *) b);
     free(b);
     printf("val = %d,ptr = %p\n",*b,(void *) b);
-    
+    */
 
     return 0;
 }
