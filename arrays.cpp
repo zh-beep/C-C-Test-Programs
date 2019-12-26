96f5aa7d87fa164e6e294e5f5338d746ddcd1bba
Zanir Habib <zanir.habib@gmail.com>
2019-12-26 02:58:48 -0600

    print.cpp: working with cout, arrays.cpp: working with arrays

---
+++ arrays.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    int a[3][2] = { {0,0}, {1,1), {2,2}, {3,3}};
+    for ( int i = 0; i < 3, i++)
+    {
+        for( int j = 0; j < 2; j++)
+	{
+        cout << "a[" << i << "] b[" <<  j << "]" + a[i][j]
+    //3 rows, 2 columns
+}	
---
+++ print.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a; 
+    a = 4;
+    cout << "hey" << a  << endl; //can't do it like python where you can add variables to print
+    return 0;
+
+}
