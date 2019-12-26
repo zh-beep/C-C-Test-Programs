116e110e296302cab8e55d327874997be2c5c8f5
Zanir Habib <zanir.habib@gmail.com>
2019-12-26 03:18:15 -0600

    Printing contents of 2D array

--- arrays.cpp
+++ arrays.cpp
@@ -1,10 +1,19 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
-    int a[3][2] = { {0,0}, {1,1), {2,2}, {3,3}};
-    for ( int i = 0; i < 3, i++)
+    int a[3][2] = { {0,0}, {1,1}, {2,2}};
+    //3 rows, 2 columns
+    for ( int i = 0; i < 3; i++)
     {
         for( int j = 0; j < 2; j++)
 	{
-        cout << "a[" << i << "] b[" <<  j << "]" + a[i][j]
-    //3 rows, 2 columns
+        cout << "a[" << i << "] b[" <<  j << "]:" << a[i][j] << endl;
+
+        }
+    }
+
+
+
 }	
